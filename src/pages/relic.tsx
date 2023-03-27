d039c83c7664f6fd9ea411a46f43e9cc14356f93
RnZ3 <RnZ3@debian-BULLSEYE-live-builder-AMD64>
2023-03-27 23:36:27 +0200

    RnZ changes

---
+++ src/pages/relic.tsx
@@ -0,0 +1,146 @@
+import {
+  Box,
+  Card,
+  Heading,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+  HStack,
+  Button,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { api } from "~/utils/api";
+import { CustomConnectButton } from "~/components/CustomConnectButton";
+import { FbBack,FbSmall } from "~/components/fbeet";
+import { useAccount } from "wagmi";
+
+const Read: NextPage = () => {
+  const account = useAccount();
+
+  console.log(account.address, account.isConnected);
+
+  return (
+    <>
+      <VStack m={5}>
+        <Heading>Relic Tool</Heading>
+        <CustomConnectButton />
+        {account.isConnected && <RelicDisplay />}
+      </VStack>
+<FbBack/>
+    </>
+  );
+};
+
+export default Read;
+
+const RelicDisplay = () => {
+  const toast = useToast();
+
+  const account = useAccount();
+
+  //const myAddr = "0xa3b926f6d2bB5507fE711847640bA2086CB11A75";
+  //const myAddr = "0xC677E71B02adc94534De993A907352f748d21143";
+  const myAddr = account?.address;
+  const relicId = 163;
+  const poolId = 2;
+
+  const relicPositions = api.relic.relicPositionsOfOwner.useQuery({
+    owner: myAddr,
+  }).data;
+
+  console.log(relicPositions);
+
+  if (relicPositions?.level.ids.length === 0) {
+    return (
+      <>
+<p><FbSmall/> no relics</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Card m={12} p={6}>
+        <HStack>
+          {relicPositions?.level.ids.map((rel, index) => {
+            return (
+              <Box key={index}>
+                <Card m={12} p={6} w={300} variant="outline">
+                  <Text>Relic #{rel}</Text>
+                  <Divider m={3} />
+                  <Text>
+                    Level: {relicPositions.level.positions[index]?.level}
+                  </Text>
+                  <Text>
+                    <FbSmall/>{" "}
+                    {relicPositions.level.positions[index]?.amount.toFixed(2)}
+                  </Text>
+                  <Text>
+                    Entry:{" "}
+                    {new Date(
+                      (relicPositions.level.positions[index]?.entry || 0) * 1000
+                    ).toDateString()}
+                  </Text>
+                  <Divider m={3} />
+                  <HStack>
+                    <Button
+                      onClick={() =>
+                        toast({
+                          title: "Split",
+                          description: `split relic #${rel}`,
+                          status: "success",
+                          duration: 5000,
+                          isClosable: true,
+                          position: "top",
+                        })
+                      }
+                    >
+                      Split
+                    </Button>
+                    <Button
+                      onClick={() =>
+                        toast({
+                          title: "Merge",
+                          description: `merge relic #${rel}`,
+                          status: "success",
+                          duration: 5000,
+                          isClosable: true,
+                          position: "top",
+                        })
+                      }
+                    >
+                      Merge
+                    </Button>
+                    <Button
+                      onClick={() =>
+                        toast({
+                          title: "Transfer",
+                          description: `transfer relic #${rel}`,
+                          status: "success",
+                          duration: 5000,
+                          isClosable: true,
+                          position: "top",
+                        })
+                      }
+                    >
+                      Transfer
+                    </Button>
+                  </HStack>
+                </Card>
+              </Box>
+            );
+          })}
+        </HStack>
+      </Card>
+    </>
+  );
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+import { extendTheme, type ThemeConfig, type ChakraTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+
+export const siteTheme: Partial<ChakraTheme> = {
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        color: mode("#222", "#CCC")(props),
+        background: mode("#F4F7FF", "#0d081c")(props),
+      },
+    }),
+  },
+  fonts: {
+    heading: "Inter, sans-serif",
+    body: "Inter, sans-serif",
+  },
+  semanticTokens: {
+    colors: {
+      bw_green: { default: "#308e61", _dark: "#4be39c" },
+      bw_red: { default: "#ed1200", _dark: "#ed1200" },
+      bg_card: { default: "#D5E0EC", _dark: "#1C2635" },
+      bg_toprow: { default: "#F4F7FF", _dark: "#0d081c" },
+    },
+  },
+  components: {
+    Progress: {
+      defaultProps: {
+        colorScheme: "green",
+      },
+    },
+  },
+};
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+export const theme = extendTheme({ ...siteTheme, config });
+export default theme;
